7a6ed8b1bce5f211aa3df560355cac8e7f00ed24
zhaozzzzbr <32559027@mail.dlut.edu.cn>
2025-12-16 16:10:45 +0800

    完成if_else.py测试

--- exercises/if_else.py
+++ exercises/if_else.py
@@ -23,4 +23,15 @@ def check_grade(score):
     - 对应的等级：优秀、良好、中等、及格、不及格
     """
     # 请在下方编写代码
+    if score >= 90:
+        return "优秀"
+    elif score >= 80:
+        return "良好"
+    elif score >= 70:
+        return "中等"
+    elif score >= 60:
+        return "及格"
+    else:
+        return "不及格"
+
     pass 
