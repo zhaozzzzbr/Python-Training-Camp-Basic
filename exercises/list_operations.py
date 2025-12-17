1e84a1d4bcd5057752865014b78624c810c5390e
zhaozzzzbr <32559027@mail.dlut.edu.cn>
2025-12-17 14:46:08 +0800

    完成list_operation.py练习

--- exercises/list_operations.py
+++ exercises/list_operations.py
@@ -20,4 +20,16 @@ def student_list_operations(students, operation, *args):
     - 操作后的学生列表
     """
     # 请在下方编写代码
+    if operation == "add":
+        students.append(args[0])
+    elif operation == "remove":
+        students.remove(args[0])
+    elif operation == "update":
+        old_name = args[0]
+        new_name = args[1]
+        if old_name in students:
+            x = students.index(old_name)
+            students[x] = new_name
+    return students
+
     pass 
