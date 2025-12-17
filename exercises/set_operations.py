a047de9657c0c68ec508897336d808b8538adfb8
zhaozzzzbr <32559027@mail.dlut.edu.cn>
2025-12-17 15:14:33 +0800

    完成set_operations.py练习

--- exercises/set_operations.py
+++ exercises/set_operations.py
@@ -20,4 +20,13 @@ def student_set_operations(set1, set2, operation):
     - 集合操作的结果
     """
     # 请在下方编写代码
+    if operation == "union":
+        return set1.union(set2)
+
+    elif operation == "intersection":
+        return set1.intersection(set2)
+    
+    elif operation == "difference":
+        return set1.difference(set2)
+
     pass
