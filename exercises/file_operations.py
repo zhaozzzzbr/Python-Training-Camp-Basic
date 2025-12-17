ecbc9c96e8785e43d60c714d5343b374a3045e75
zhaozzzzbr <32559027@mail.dlut.edu.cn>
2025-12-17 16:25:59 +0800

    完成file_operations.py练习

--- exercises/file_operations.py
+++ exercises/file_operations.py
@@ -19,6 +19,10 @@ def read_file(file_path):
     """
     # 请在下方编写代码
     # 使用open()函数打开文件并读取内容
+    with open(file_path, 'r',encoding='utf-8') as file:
+        content = file.read()
+    return content
+
     pass
 
 def write_file(file_path, content):
@@ -34,4 +38,11 @@ def write_file(file_path, content):
     """
     # 请在下方编写代码
     # 使用with语句和open()函数写入内容到文件
+    try:
+         with open(file_path, 'w',encoding='utf-8') as file:
+             file.write(content) 
+         return True 
+    except Exception as e:
+         print(f"写入文件时出错: {e}")
+         return False  
     pass 
